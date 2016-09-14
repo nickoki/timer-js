e062353be1102ad1c5e6e024b89fc228bce54bc7
Nick Oki <nick.oki@live.com>
2016-09-14 12:24:28 -0400

    Reset Button

--- timers.js
+++ timers.js
@@ -4,10 +4,13 @@ When "Reset" is clicked, the text should return to "Stop Watch"
 When "Pause" is clicked, the text should say "Time elapsed: 1", but stop incrementing.
 */
 
-var time = 0;
+// TODO switch start button to reset button after click
 
+var time = 0;
+var timerLog = [];
+
+// Start button
 $(start).on('click', function() {
-	time = 0;
 	$(timer).html("Time elapsed: ");
 	$(timer).append('<span></span>');
 	$(timer).children().html(time);
@@ -15,9 +18,25 @@ $(start).on('click', function() {
 	incrementTime();
 });
 
+// Reset button
+$(reset).on('click', function() {
+	$(timer).html("Stop Watch");
+	clearInterval(timerLog[0]);
+	timerLog = [];
+	time = 0;
+});
+
+// Pause button
+
+
 // Time Increment function, every 1 second
 function incrementTime() {
-	setInterval(setTime, 1000);
+	// Check for multiple timers
+	if (timerLog.length > 0) {
+		throw "Only One Timer Allowed";
+	} else {
+		timerLog.push(setInterval(setTime, 1000));
+	}
 }
 
 function setTime() {
@@ -25,3 +44,21 @@ function setTime() {
 	console.log(time);
 	$(timer).children().html(time);
 }
+
+
+
+
+/*
+timers = [];
+start.on("click", function(){
+  timers.push(setInterval(singAnnoyingSong, 500));
+  console.log(timers);
+});
+
+stop.on("click", function(){
+  for (var i = 0; i < timers.length; i++) {
+    clearInterval(timers[i])
+  }
+  timers = [];
+});
+*/
